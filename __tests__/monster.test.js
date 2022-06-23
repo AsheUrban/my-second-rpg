3c5d7f9f69829a0093fd242a3ad143637effc536
FrankTimmons <franktimmonspdx@gmail.com>
2022-06-23 11:09:27 -0700

    Get a good fail on the second monster test.
    Co-authored-by: JaredGran <jared.carper@gmail.com>

--- __tests__/monster.test.js
+++ __tests__/monster.test.js
@@ -2,7 +2,11 @@ import { Monster } from '../src/monster.js';
 
 describe('Monster', () => {
   test('should create new monster object when called on', () => {
-    const newMonst = new Monster();
+    let newMonst = new Monster();
     expect(typeof newMonst).toEqual("object");
+  });
+  test('should create a monster object with stats', () => {
+    let newMonst = new Monster();
+    expect(newMonst.hp).toEqual(1);
   });
 }); 
