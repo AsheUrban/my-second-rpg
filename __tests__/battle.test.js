25b09c185286bb0fd8effc763e38a94bc909e5fa
FrankTimmons <franktimmonspdx@gmail.com>
2022-06-23 13:24:30 -0700

    Good fail the second battle test.
    Co-authored-by: Peter Vang <vang_peter06@yahoo.com>
    Co-authored-by: JaredGran <jared.carper@gmail.com>

--- __tests__/battle.test.js
+++ __tests__/battle.test.js
@@ -9,7 +9,13 @@ describe('Battle', () => {
     newCharWar = new Character("Warrior");
     monster = new Monster("Monster");
   });
+
   test('should take in a character and monster to engage in battle', () => {
     expect(battle(newCharWar, monster)).toEqual("Warrior is battling Monster")
+  });
+
+  test('should take the players attack value away from the monsters health', () => {
+    battle(newCharWar, monster);
+    expect(monster.hp).toEqual(0);
   });
 });
