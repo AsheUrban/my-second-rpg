5459eb759942905d651f127c88e9ed094a8c3e02
FrankTimmons <franktimmonspdx@gmail.com>
2022-06-23 13:41:35 -0700

    Pass third battle test.
    Co-authored-by: Peter Vang <vang_peter06@yahoo.com>
    Co-authored-by: JaredGran <jared.carper@gmail.com>

--- src/battle.js
+++ src/battle.js
@@ -1,7 +1,10 @@
 export const battle = (player, enemy) => {
   enemy.hp -= player.bag[0].damage;
-  if(enemy.hp < 0){
+
+  if(enemy.hp <= 0){
     enemy.hp = 0;
+    enemy.status = "Dead"
   }
+
   return player.charClass + " is battling " + enemy.name;
 } 
